f70605d2f9d60302edf8fb046337fab6e2f3fa73
ncsot <ncsot@ya.ru>
2016-05-14 12:55:48 +0000

    del

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 sudo rm -r /etc/nginx/sites-enabled/default
-sudo ln -sf ~/web/default.conf /etc/nginx/sites-enabled/stepic.conf
+sudo ln -sf ~/web/etc/default.conf /etc/nginx/sites-enabled/stepic.conf
 sudo nginx start
