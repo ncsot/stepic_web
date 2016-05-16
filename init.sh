092a18ddaf43c0bfa76d7ab6221056124d4f5ed6
ncsot <ncsot@ya.ru>
2016-05-16 16:23:41 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 sudo rm -r /etc/nginx/sites-enabled/default
-sudo ln -sf ~/web/etc/default.conf /etc/nginx/sites-enabled/stepic.conf
+sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/stepic.conf
 sudo /etc/init.d/nginx start
+sudo ln -sf ~/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
