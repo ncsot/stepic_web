b1efef632c81cda89f19585b2725f51cf75be2f8
ncsot <ncsot@ya.ru>
2016-05-16 16:55:10 +0300

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,12 +1,1 @@
-CONFIG = {                                                                      
-    # 'mode': 'wsgi',                                                           
-    'working_dir': '/home/web/',                                                
-    # 'python': '/usr/bin/python',                                              
-    'args': (                                                                   
-        '--bind=0.0.0.0:8080',                                                
-        '--workers=16',                                                         
-        '--timeout=60',                                                         
-        'hello:app',                                                            
-    ),                                                                          
-}                                                                               
-    
+bind = "0.0.0.0:8080"
